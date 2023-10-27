d76d929872d866df8ce263a79a54e069197f26ff
Gareth Murden <Gareth.Murden@dyson.com>
2023-10-27 11:12:39 +0100

    control simulator

--- control.py
+++ control.py
@@ -1,24 +1,56 @@
-
-# https://github.com/modmypi/Rotary-Encoder/blob/master/rotary_encoder.py
-# https://learn.adafruit.com/adafruit-dc-and-stepper-motor-hat-for-raspberry-pi/using-stepper-motors
+import os
+import time
+import threading
 
 DIAL_POSITION = 0
 MOTOR_POSITION = 0
 
 def convert(dial):
-	motor = dial * 8
-	return motor
+    motor = dial * 8
+    return motor
 
-'''
-Flow:
-- Dial turned X steps in Y direction
-- Calculate new dial position P
-- Covnert P to motor target position T
-- Send signal to rotate motor in direction Y until T reached
-'''
+def dial():
+    global DIAL_POSITION
+    while True:
+        os.system('cls')
+        print()
+        print(f' DIAL_POSITION:  {str(DIAL_POSITION).rjust(3)}')
+        print(f' MOTOR_POSITION: {str(MOTOR_POSITION).rjust(3)}')
+        direction = input(' Turn Clockwise or Anticlockwise? ')
+        if len(direction) > 0:
+            if direction[0].upper() == 'C':
+                DIAL_POSITION += 1
+                if DIAL_POSITION > 23:
+                    DIAL_POSITION = 0
+            elif direction[0].upper() == 'A':
+                DIAL_POSITION -= 1
+                if DIAL_POSITION < 0:
+                    DIAL_POSITION = 23
+
+def motor(interrupt):
+    global MOTOR_POSITION
+    while True:
+        if MOTOR_POSITION < convert(DIAL_POSITION):
+            MOTOR_POSITION += 1
+        elif MOTOR_POSITION > convert(DIAL_POSITION):
+            MOTOR_POSITION -=1
+        if interrupt.is_set():
+            break
+        else:
+            time.sleep(1)
+
+def main():
+    interrupt = threading.Event()
+    motor_thread = threading.Thread(
+        target=motor,
+        args=(interrupt,)
+    )
+    motor_thread.start()
+    try:
+        dial()
+    except KeyboardInterrupt:
+        interrupt.set()
+
 
 if __name__ == '__main__':
-	print(' dial | motor')
-	for x in range(0, 24):
-		print(f'   {str(x).rjust(2)} |   {str(convert(x)).rjust(3)}')
-
+    main()
