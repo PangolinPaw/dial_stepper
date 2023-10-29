098b758f5ed4fc232621d4dcc6ae97332d934608
Pangolin_PC <pangolinpaw@hotmail.co.uk>
2023-10-29 09:05:04 +0000

    prepare for 2nd dial

--- control.py
+++ control.py
@@ -3,51 +3,53 @@ import time
 import threading
 from RPi import GPIO
 
-dt = 18
-clk = 17
+dt_A = 18
+clk_A = 17
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # CLK
-GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # DT
+GPIO.setup(clk_A, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # clk_A
+GPIO.setup(dt_A, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # dt_A
 
-DIAL_POSITION = 0
-MOTOR_POSITION = 0
+DIAL_A = 0
+DIAL_B = 0
+MOTOR_A = 0
+MOTOR_B = 0
 
 def convert(dial):
     motor = dial * 8
     return motor
 
 def dial():
-    global DIAL_POSITION
-    clkLastState = GPIO.input(clk)
+    global DIAL_A
+    clk_A_last_state = GPIO.input(clk_A)
     while True:
-        os.system('clear')
-        print()
-        print(f' DIAL_POSITION:  {str(DIAL_POSITION).rjust(3)}')
-        print(f' MOTOR_POSITION: {str(MOTOR_POSITION).rjust(3)}')
-
-        clkState = GPIO.input(clk)
-        dtState = GPIO.input(dt)
-        if clkState != clkLastState:
-#            print(f' {dtState = }, {clkState = }')
-            if dtState != clkState:
-                DIAL_POSITION += 1
-                if DIAL_POSITION > 23:
-                    DIAL_POSITION = 0
+        clk_A_state = GPIO.input(clk_A)
+        dt_A_state = GPIO.input(dt_A)
+        if clk_A_state != clk_A_last_state:
+            if dt_A_state != clk_A_state:
+                DIAL_A += 1
+                if DIAL_A > 23:
+                    DIAL_A = 0
             else:
-                DIAL_POSITION -= 1
-                if DIAL_POSITION < 0:
-                    DIAL_POSITION = 23
-            clkLastState = clkState
+                DIAL_A -= 1
+                if DIAL_A < 0:
+                    DIAL_A = 23
+            clk_A_last_state = clk_A_state
             time.sleep(0.01)
 
 def motor(interrupt):
-    global MOTOR_POSITION
+    global MOTOR_A
     while True:
-        if MOTOR_POSITION < convert(DIAL_POSITION):
-            MOTOR_POSITION += 1
-        elif MOTOR_POSITION > convert(DIAL_POSITION):
-            MOTOR_POSITION -=1
+        os.system('clear')
+        print()
+        print(f' DIAL_A:  {str(DIAL_A).rjust(3)}')
+        print(f' MOTOR_A: {str(MOTOR_A).rjust(3)}')
+        print(f' DIAL_B:  {str(DIAL_B).rjust(3)}')
+        print(f' MOTOR_B: {str(MOTOR_A).rjust(3)}')
+        if MOTOR_A < convert(DIAL_A):
+            MOTOR_A += 1
+        elif MOTOR_A > convert(DIAL_A):
+            MOTOR_A -=1
         if interrupt.is_set():
             break
         else:
