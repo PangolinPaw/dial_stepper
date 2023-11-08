952e31f4eb3cc4c6fb389e1379868e05d42ccb4d
Russell Penn <russellpenn123@gmail.com>
2023-11-08 20:17:00 +0000

    plays static between solutions

--- audio/two-at-once.py
+++ audio/two-at-once.py
@@ -8,7 +8,7 @@ from PyQt5.QtCore import Qt
 class RadioFuzzApp(QMainWindow):
     def __init__(self, audio_file_path1, audio_file_path2):
         super().__init__()
-        self.setWindowTitle('Audio Mixer')
+        self.setWindowTitle('Audio Mixer with Static')
         self.audio_file_path1 = audio_file_path1
         self.audio_file_path2 = audio_file_path2
         self.initUI()
@@ -20,7 +20,7 @@ class RadioFuzzApp(QMainWindow):
         self.setCentralWidget(main_widget)
         layout = QVBoxLayout(main_widget)
 
-        # Slider for blending audio
+        # Slider for blending audio and static
         self.slider = QSlider(Qt.Horizontal)
         self.slider.setRange(0, 35)  # Set range from 0 to 35
         self.slider.setValue(0)  # Start with only the first clip playing
@@ -39,13 +39,12 @@ class RadioFuzzApp(QMainWindow):
         min_len = min(len(self.data1), len(self.data2))
         self.data1, self.data2 = self.data1[:min_len], self.data2[:min_len]
 
-        # Initial mix is just the first audio clip
-        self.mix_ratio = 0  # Ratio of how much of each audio clip to play
-
         # Audio stream setup
         self.stream = sd.OutputStream(samplerate=self.fs1, channels=self.data1.shape[1], callback=self.audio_callback)
         self.stream.start()
 
+        # Initial mix is just the first audio clip
+        self.mix_ratio = 0  # Ratio of how much of each audio clip to play
         self.position = 0
 
     def adjust_mix(self):
@@ -61,9 +60,14 @@ class RadioFuzzApp(QMainWindow):
             chunk_end = len(self.data1)
             self.position = 0  # Loop back to the beginning
 
+        # Calculate the mix and the amount of static
+        static_intensity = 1 - abs(self.mix_ratio - 0.5) * 2
+        static = np.random.normal(0, static_intensity, (frames, self.data1.shape[1])).astype('float32')
+
         # Mix the audio according to the slider position
         mix_chunk = ((1 - self.mix_ratio) * self.data1[self.position:chunk_end] +
-                     self.mix_ratio * self.data2[self.position:chunk_end]).astype('float32')
+                     self.mix_ratio * self.data2[self.position:chunk_end] +
+                     static).astype('float32')
 
         outdata[:] = mix_chunk
 
