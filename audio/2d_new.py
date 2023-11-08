b28507fa72c8eb434bcf5de7d1186939c1bcd980
Russell Penn <russellpenn123@gmail.com>
2023-11-08 21:08:07 +0000

    closer with 2D

--- audio/2d_new.py
+++ audio/2d_new.py
@@ -1,111 +1,115 @@
 import sys
 import numpy as np
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtGui import QPainter, QPen, QBrush, QColor, QFont
+from PyQt5.QtCore import Qt, QRectF, QPointF
 import sounddevice as sd
 import soundfile as sf
-from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsEllipseItem
-from PyQt5.QtCore import Qt, QRectF, QPointF
 
-class RadioFuzzApp(QApplication):
-    def __init__(self, args):
-        super().__init__(args)
-        self.main_window = FuzzWindow()
-        self.main_window.show()
+# Replace these with the paths to your actual audio files
+AUDIO_CLIP_1 = "rock.wav"
+AUDIO_CLIP_2 = "piano.wav"
 
-class FuzzWindow(QGraphicsView):
-    def __init__(self):
-        super().__init__()
-        self.scene = QGraphicsScene(self)
-        self.setScene(self.scene)
-        self.setSceneRect(QRectF(self.viewport().rect()))
+# Load audio clips
+data1, fs1 = sf.read(AUDIO_CLIP_1)
+data2, fs2 = sf.read(AUDIO_CLIP_2)
 
-        # Load audio files and fuzz
-        self.data1, self.fs1 = self.load_audio('rock.wav')
-        self.data2, self.fs2 = self.load_audio('piano.wav')
-        self.fuzz, self.fs_fuzz = self.load_audio('radio_16.wav')
+# Ensure same sample rate for both clips
+assert fs1 == fs2
 
-         # Coordinates where each audio clip plays clearly
-        self.source1_position = np.array([100, 100])  # Adjust as needed
-        self.source2_position = np.array([200, 200])  # Adjust as needed
+# Mix audio depending on the position on the plane
+def mix_audio(position, clip1, clip2, vol1, vol2):
+    # Add radio fuzz effect by introducing random noise
+    fuzz = np.random.normal(0, 0.005, clip1.shape).astype(clip1.dtype)
+    # Mix the clips with their respective volumes
+    return (clip1 * vol1 + clip2 * vol2 + fuzz * (1 - vol1 - vol2))
 
-        # Ensure all sample rates are the same
-        assert self.fs1 == self.fs2 == self.fs_fuzz, "Sample rates do not match!"
+class FuzzWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(FuzzWindow, self).__init__(parent)
+        self.setMinimumSize(400, 400)
+        self.marker1 = QPointF(100, 100)  # Point for audio clip 1
+        self.marker2 = QPointF(300, 300)  # Point for audio clip 2
+        self.dragging = False
+        self.current_pos = QPointF(self.width() / 2, self.height() / 2)
 
-        # Initialize the ellipse in the center
-        self.initialize_ellipse()
-        self.initialize_markers()
+        self.initUI()
 
+        # Audio stream setup
+        self.stream = sd.OutputStream(
+            samplerate=fs1,
+            channels=data1.shape[1] if data1.ndim > 1 else 1,
+            blocksize=1024,
+            callback=self.audio_callback
+        )
 
-        # Set up audio stream
-        self.stream = sd.OutputStream(callback=self.audio_callback, samplerate=self.fs1, channels=2)
+        # Start the audio stream
+        self.start_audio_stream()
+
+    def initUI(self):
+        # Your UI initialization code
+        self.setWindowTitle('Audio Fuzz Mixer')
+        self.show()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        # Draw markers
+        painter.setPen(QPen(Qt.red, 8, Qt.SolidLine))
+        painter.drawPoint(self.marker1)
+        painter.drawPoint(self.marker2)
+
+        # Draw draggable position
+        painter.setBrush(QBrush(Qt.white, Qt.SolidPattern))
+        painter.drawEllipse(self.current_pos, 10, 10)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.dragging = True
+            self.current_pos = event.pos()
+            self.update()
+
+    def mouseMoveEvent(self, event):
+        if self.dragging:
+            self.current_pos = event.pos()
+            self.update()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.dragging = False
+
+    def calculate_volume(self, current_pos, marker_pos):
+        distance = np.linalg.norm(np.array([current_pos.x(), current_pos.y()]) - np.array([marker_pos.x(), marker_pos.y()]))
+        max_distance = np.linalg.norm([self.width(), self.height()])
+        volume = 1 - (distance / max_distance)
+        return max(min(volume, 1), 0)
+
+    def start_audio_stream(self):
+        self.position = 0
         self.stream.start()
 
-        # Current position in the audio file
-        self.position = 0
-
-    def initialize_ellipse(self):
-        ellipse_radius = 10
-        scene_center = self.sceneRect().center()
-        self.ellipse = QGraphicsEllipseItem(scene_center.x() - ellipse_radius, scene_center.y() - ellipse_radius, ellipse_radius * 2, ellipse_radius * 2)
-        self.ellipse.setBrush(Qt.white)
-        self.ellipse.setFlag(QGraphicsEllipseItem.ItemIsMovable)
-        self.scene.addItem(self.ellipse)
-        
-    def initialize_markers(self):
-        self.marker1 = self.create_marker(self.source1_position, Qt.red)
-        self.marker2 = self.create_marker(self.source2_position, Qt.blue)
-        
-        # Add markers to the scene
-        self.scene.addItem(self.marker1)
-        self.scene.addItem(self.marker2)
-
-    def create_marker(self, position, color):
-        marker_radius = 5
-        marker = QGraphicsEllipseItem(position[0] - marker_radius, position[1] - marker_radius, marker_radius * 2, marker_radius * 2)
-        marker.setBrush(color)
-        return marker
-
-    def load_audio(self, filepath):
-        data, fs = sf.read(filepath, always_2d=True)
-        return data, fs
-
     def audio_callback(self, outdata, frames, time, status):
-        chunk_end = self.position + frames
-        self.check_buffer_length(chunk_end, frames)
+        if status:
+            print(status, file=sys.stderr)
+        chunk_start = self.position
+        chunk_end = chunk_start + frames
+        if chunk_end > len(data1):
+            chunk_end = len(data1)
+            self.position = 0  # Loop back to the beginning
 
-        mix = self.mix_audio(frames)
+        vol1 = self.calculate_volume(self.current_pos, self.marker1)
+        vol2 = self.calculate_volume(self.current_pos, self.marker2)
 
-        outdata[:frames] = mix
+        clip1 = data1[chunk_start:chunk_end]
+        clip2 = data2[chunk_start:chunk_end]
+
+        mix = mix_audio(self.position, clip1, clip2, vol1, vol2)
+
+        outdata[:] = mix
         self.position += frames
-        if self.position >= len(self.data1):
-            self.position = 0
-
-    def check_buffer_length(self, chunk_end, frames):
-        if chunk_end > len(self.data1):
-            chunk_end = len(self.data1)
-            frames = chunk_end - self.position
-
-    def mix_audio(self, frames):
-        # Calculate volume and mix based on the position of the ellipse
-        pos = np.array([self.ellipse.pos().x(), self.ellipse.pos().y()])
-        source1_position = np.array([100, 100])  # Example position for source 1
-        source2_position = np.array([200, 200])  # Example position for source 2
-
-        # Calculate distances to source positions
-        distance_to_source1 = np.linalg.norm(pos - source1_position)
-        distance_to_source2 = np.linalg.norm(pos - source2_position)
-
-        # Fuzz amount based on distance to sources
-        fuzz_amount = max(0, 1 - (1 / (distance_to_source1 + distance_to_source2 + 1)))
-
-        # Blend audio sources and fuzz based on distances
-        audio1_volume = 1 - distance_to_source1 / (distance_to_source1 + distance_to_source2 + 1)
-        audio2_volume = 1 - distance_to_source2 / (distance_to_source1 + distance_to_source2 + 1)
-
-        mix = (self.data1[self.position:self.position + frames] * audio1_volume +
-               self.data2[self.position:self.position + frames] * audio2_volume +
-               self.fuzz[self.position:self.position + frames] * fuzz_amount)
-
-        return mix
+        if chunk_end == len(data1):
+            self.position = 0  # Loop back to the beginning
 
     def closeEvent(self, event):
         self.stream.stop()
@@ -113,5 +117,6 @@ class FuzzWindow(QGraphicsView):
         super().closeEvent(event)
 
 if __name__ == '__main__':
-    app = RadioFuzzApp(sys.argv)
+    app = QApplication(sys.argv)
+    mainWin = FuzzWindow()
     sys.exit(app.exec_())
