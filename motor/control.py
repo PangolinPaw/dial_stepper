a8a8fccce898a586a6b818c7ef85ff2e1bac0808
Russell Penn <russellpenn123@gmail.com>
2023-11-16 14:58:28 +0000

    chat gpt help me

--- motor/control.py
+++ motor/control.py
@@ -5,23 +5,14 @@ from RPi import GPIO
 
 # Initialise GPIO & motor controllers
 GPIO.setmode(GPIO.BCM)
-kit = MotorKit() # Lower controller (motors A & B)
-kit2 = MotorKit(address=0x61) # Upper controller (motor C)
+kit = MotorKit()  # Lower controller (motors A & B)
+kit2 = MotorKit(address=0x61)  # Upper controller (motor C)
 
 # Motor positions for use in other scripts
 MOTORS = {
-    'a':{
-        'position':0,
-        'motor':kit.stepper1
-    },
-    'b':{
-        'position':0,
-        'motor':kit.stepper2
-    },
-    'c':{
-        'position':0,
-        'motor':kit2.stepper1
-    }
+    'a': {'position': 0, 'motor': kit.stepper1},
+    'b': {'position': 0, 'motor': kit.stepper2},
+    'c': {'position': 0, 'motor': kit2.stepper1}
 }
 
 # Ratio between dial steps & motor steps
@@ -29,49 +20,24 @@ RATIO = 8
 
 # Level of 'smoothing' applied to dial input
 SMOOTHING = 8
+DEBOUNCE_TIME = 0.01  # Debounce time in seconds
 
-def dial_smooting(dial, signal):
-    '''Dial can 'wobble' between clockwise & anticlockwise so this function smoothes
-    the changes before they're used as signals for motor movement'''
+def dial_smoothing(dial, signal):
     dial['buffer'].append(signal)
     if len(dial['buffer']) > SMOOTHING:
         del dial['buffer'][0]
-        if sum(dial['buffer']) > 1:
+        buffer_sum = sum(dial['buffer'])
+        if buffer_sum > SMOOTHING / 2:
             return 1
-        elif sum(dial['buffer']) < 1:
+        elif buffer_sum < -SMOOTHING / 2:
             return -1
-        else:
-            return 0
-    else:
-        return 0
+    return 0
 
 def initialise():
-    '''Set up GPIO pins and initial positions for dials'''
     dials = {
-        'a':{
-            'clk':20,
-            'dt':21,
-            'position':0,
-            'buffer':[],
-            'clk_last_state':False,
-            'motor':kit.stepper1
-        },
-        'b':{
-            'clk':19,
-            'dt':26,
-            'position':0,
-            'buffer':[],
-            'clk_last_state':False,
-            'motor':kit.stepper2
-        },
-        'c':{
-            'clk':6,
-            'dt':5,
-            'position':0,
-            'buffer':[],
-            'clk_last_state':False,
-            'motor':kit2.stepper1
-        }
+        'a': {'clk': 20, 'dt': 21, 'position': 0, 'buffer': [], 'clk_last_state': False, 'motor': kit.stepper1},
+        'b': {'clk': 19, 'dt': 26, 'position': 0, 'buffer': [], 'clk_last_state': False, 'motor': kit.stepper2},
+        'c': {'clk': 6, 'dt': 5, 'position': 0, 'buffer': [], 'clk_last_state': False, 'motor': kit2.stepper1}
     }
     for dial in dials:
         GPIO.setup(dials[dial]['clk'], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -79,60 +45,33 @@ def initialise():
         dials[dial]['clk_last_state'] = GPIO.input(dials[dial]['clk'])
     return dials
 
-def move_motor(motor_name, motor, direction):
+def move_motor(motor_name, motor, direction, steps):
     global MOTORS
-    change = 0
-    for x in range(RATIO):
-        motor.onestep(
-            direction=direction
-        )
-        change += 1
-    new_position = MOTORS[motor_name]['position'] + change
-    MOTORS[motor_name]['position'] = new_position % 200
+    for _ in range(steps):
+        motor.onestep(direction=direction)
+    MOTORS[motor_name]['position'] = (MOTORS[motor_name]['position'] + (1 if direction == stepper.FORWARD else -1) * steps) % 200
 
 def read_dials():
-    '''Receive signals from rotary encoders & determine rotation direction
-    & distance'''
     dials = initialise()
+    last_dial_check_time = time.time()
 
     while True:
         for dial in dials:
+            if time.time() - last_dial_check_time < DEBOUNCE_TIME:
+                continue
+
             clk_state = GPIO.input(dials[dial]['clk'])
-            dt_state = GPIO.input(dials[dial]['dt'])
-            change = 0
             if clk_state != dials[dial]['clk_last_state']:
-                if dt_state != clk_state:
-                    change = dial_smooting(dials[dial], 1)
-                    move_motor(
-                        dial,
-                        dials[dial]['motor'],
-                        stepper.FORWARD
-                    )
-                else:
-                    change = dial_smooting(dials[dial], -1)
-                    move_motor(
-                        dial,
-                        dials[dial]['motor'],
-                        stepper.BACKWARD
-                    )
-                print(f"[ {dial.upper()} ] : dial ={str(dials[dial]['position']).rjust(3)}\t motor ={str(MOTORS[dial]['position']).rjust(3)}")
+                dt_state = GPIO.input(dials[dial]['dt'])
+                change = dial_smoothing(dials[dial], 1 if dt_state != clk_state else -1)
+                if change != 0:
+                    move_motor(dial, dials[dial]['motor'], stepper.FORWARD if change > 0 else stepper.BACKWARD, abs(change))
+                    print(f"[ {dial.upper()} ] : dial = {str(dials[dial]['position']).rjust(3)}\t motor = {str(MOTORS[dial]['position']).rjust(3)}")
+                last_dial_check_time = time.time()
+
             dials[dial]['clk_last_state'] = clk_state
-
             dials[dial]['position'] = (dials[dial]['position'] + change) % 24
-        time.sleep(0.01)
-
-def set_motor(motor_name, position):
-    '''Intended for demo mode, move motor to specified position'''
-    global MOTORS
-    while position != MOTORS[motor_name]['position']:
-        if position > MOTORS[motor_name]['position']:
-            MOTORS[motor_name]['motor'].onestep(direction=stepper.FORWARD)
-            MOTORS[motor_name]['position'] += 1
-        else:
-            MOTORS[motor_name]['motor'].onestep(direction=stepper.BACKWARD)
-            MOTORS[motor_name]['position'] -= 1
         time.sleep(0.01)
 
 if __name__ == '__main__':
     read_dials()
-
